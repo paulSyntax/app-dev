dc85af8677d58b74525c3d51d971e05afbea0395
jpcalutan <jpcaluts@gmail.com>
2024-04-20 18:12:10 +0800

    added course to the code

--- Prog1.java
+++ Prog1.java
@@ -1,6 +1,9 @@
 public class Prog1 {
 public static void main(String[] args) {
-System.out.println("PATEROS TECHNOLOGICAL COLLEGE");
+System.out.println("PATEROS");
+System.out.println("TECHNOLOGICAL");
+System.out.println("COLLEGE");
+System.out.println("COURSE: BSIT");
 	}
 
 }
